18099dc7eb6ee4cbcdba34c2869c8e5a28eeec0e
awaji.re46893rgy@gmail.com <github>
2021-08-14 12:08:31 +0900

    reply

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-  <ul>
+  <div class="container">
     @foreach($posts as $post)
-      <li>{{ $post->user_id }}: {{ $post->body }}</li>
+      <div class="card">
+        <div class="card-header">{{ $post->user->name }}</div>
+        <div class="card-body">
+          <p class="card-text">{{ $post->body }}</p>
+
+          @if(Auth::id() === $post->user_id)
+            <form method="POST" action="{{ route('posts.delete', $post->id) }}">
+              @csrf
+              <button type="submit" class="btn btn-danger">削除</button>
+            </form>
+          @endif
+        </div>
+      </div>
     @endforeach
-  </ul>
+  </div>
 @endsection
